558843b35bd0d0bb2dbcc348da983eb01713b391
lungaho ramsey <lungahoramsey2005@gmail.com>
2025-03-01 10:54:53 +0300

    assignment.kt

---
+++ assignment.kt
@@ -0,0 +1,22 @@
+//Task :
+// Write program that takes 3 numbers
+// finds the greatest
+// OUTPUT : The Greatest Number is:____
+
+fun main (){
+    val num1=30
+    val num2=10
+    val num3=20
+    if (num3>num2 && num3>num1){
+        println("$num3 is The Greatest number")
+    }
+    else if(num2>num3 && num2>num1 ){
+        println("$num2 is The Greatest number")
+
+    }
+    else {
+        println("$num1 is The Greatest number")
+
+
+}
+}
