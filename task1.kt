2bf705114bc1d104fc05f4f3a3178c95fca7bdea
lungaho ramsey <lungahoramsey2005@gmail.com>
2025-03-06 09:01:10 +0300

    task1.kt

---
+++ task1.kt
@@ -0,0 +1,14 @@
+fun main (){
+    var age:Int=18
+    var Vage:Int=17
+    var message1="old enough to vote"
+    var message2="Not old enough to vote"
+    
+    if (Vage>=age){
+        println(message1)
+        
+    }
+    else{
+        println(message2)
+    }
+}
